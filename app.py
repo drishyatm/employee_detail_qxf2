57677afc86d5bf44a5e1a8e0b827199004d5ab19
Drishya <drishya.tm@qxf2.com>
2021-06-04 22:33:53 +0530

    modified the file for crud operations

--- app.py
+++ app.py
@@ -1,71 +1,94 @@
-from flask import Flask , render_template, jsonify, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+app = Flask(__name__)
+app.secret_key = "000d88cd9d90036ebdd237eb6b0db000"
+
+#SqlAlchemy Database Configuration With Mysql
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Employees.sqlite3'
-	
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
+
+#Creating model table for our CRUD database
 class Employees(db.Model):
-    # Defines the Table Name employees
-    __tablename__ = "employees"
-    
-    # Makes four columns into the table id, name, email,department
-    
-    _id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(20), nullable=False)
-    email = db.Column(db.String(100), nullable=False)
-    department = db.Column(db.String(20), nullable=False)
-    #department = db.relationship('Department', backref='dep_id',lazy=True)
-    
-    
-    # A constructor function where we will pass the name and email of a employees and it gets add as a new entry in the table.
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    department = db.Column(db.String(100))
+
+
     def __init__(self, name, email, department):
+
         self.name = name
         self.email = email
         self.department = department
-        
-class Department(db.Model):        
-    __tablename__ = "department"
-    
-    # Makes two columns into the table id,department
-    
-    dep_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    department = db.Column(db.String(20), nullable=False)
-    
-    
-    # A constructor function where we will pass the department of employees and it gets add as a new entry in the table.
-    def __init__(self,department):
-        self.department = department
-                
-
-# Control will come here and then gets redirect to the index function
-@app.route("/")
-def home():
-	return redirect(url_for('index'))
 
 
-@app.route("/index", methods = ["GET", "POST"])
-def index():
-    if request.method == 'POST': 
-        data = request.form # request the data from the form in index.html file
-        name = data["name"]
-        email = data["email"]
-        department = data["department"]
-        
-        new_data = Employees(name, email, department)
-        db.session.add(new_data)
+
+#This is the index route where we are going to
+#query on all our employee data
+@app.route('/')
+def Index():
+    all_data = Employees.query.all()
+
+    return render_template("index.html", employees = all_data)
+
+
+
+#this route is for inserting data to mysql database via html forms
+@app.route('/insert', methods = ['POST'])
+def insert():
+
+    if request.method == 'POST':
+
+        name = request.form['name']
+        email = request.form['email']
+        department = request.form['department']
+
+
+        my_data = Employees(name, email, department)
+        db.session.add(my_data)
         db.session.commit()
-        
-        user_data = Employees.query.all()
-        
-        return render_template("index.html", user_data = user_data) # passes user_data variable into the index.html file.
-    
-    return render_template("index.html")
-	
 
-if __name__=="__main__":
-	db.create_all()
-	app.run(debug=True)
+        flash("Employee Inserted Successfully")
+
+        return redirect(url_for('Index'))
+
+
+#this is our update route where we are going to update our employee
+@app.route('/update', methods = ['GET', 'POST'])
+def update():
+
+    if request.method == 'POST':
+        my_data = Employees.query.get(request.form.get('id'))
+
+        my_data.name = request.form['name']
+        my_data.email = request.form['email']
+        my_data.department = request.form['department']
+
+        db.session.commit()
+        flash("Employee Updated Successfully")
+
+        return redirect(url_for('Index'))
+
+
+#This route is for deleting our employee
+@app.route('/delete/<id>/', methods = ['GET', 'POST'])
+def delete(id):
+    my_data = Employees.query.get(id)
+    db.session.delete(my_data)
+    db.session.commit()
+    flash("Employee Deleted Successfully")
+
+    return redirect(url_for('Index'))
+
+ 
+if __name__ == "__main__":
+    db.create_all()
+    app.run(debug=True)
+    
